ff29787e7975696718f5a1c32c2b620448d4e228
DenisNikolski <dzianisnikolski@gmail.com>
2021-04-08 18:47:46 +0300

    implement displayer

---
+++ lib/displayer.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Displayer
+  ALL_VIEWS_HEADING_MGS = 'list of webpages with most page views ordered from most pages views to less page views'
+  ALL_VIEWS_LINE_MSG = 'visits'
+  UNIQ_VIEWS_HEADING_MGS = 'list of webpages with most unique page views also ordered'
+  UNIQ_VIEWS_LINE_MSG = 'unique views'
+
+  def display(sorted_by_views, sorted_by_uniq_views)
+    puts ALL_VIEWS_HEADING_MGS
+    write_statistics_to_console(sorted_by_views, :views, ALL_VIEWS_HEADING_MGS)
+    puts UNIQ_VIEWS_HEADING_MGS
+    write_statistics_to_console(sorted_by_uniq_views, :uniq_views, UNIQ_VIEWS_HEADING_MGS)
+  end
+
+  private
+
+  def write_statistics_to_console(page_views, counter_key, line_message)
+    page_views.each { |page_view| puts "#{page_view.page} #{page_view[counter_key]} #{line_message}" }
+    puts
+  end
+end

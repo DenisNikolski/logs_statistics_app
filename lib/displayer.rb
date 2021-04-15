13337bc5443c609b3d75936f4fd9aaf0a6af05bf
DenisNikolski <dzianisnikolski@gmail.com>
2021-04-15 22:49:39 +0300

    refactor Displayer

--- lib/displayer.rb
+++ lib/displayer.rb
@@ -1,22 +1,9 @@
 # frozen_string_literal: true
 
 class Displayer
-  ALL_VIEWS_HEADING_MGS = 'list of webpages with most page views ordered from most pages views to less page views'
-  ALL_VIEWS_LINE_MSG = 'visits'
-  UNIQ_VIEWS_HEADING_MGS = 'list of webpages with most unique page views also ordered'
-  UNIQ_VIEWS_LINE_MSG = 'unique views'
-
-  def display(sorted_by_views, sorted_by_uniq_views)
-    puts ALL_VIEWS_HEADING_MGS
-    write_statistics_to_console(sorted_by_views, :views, ALL_VIEWS_LINE_MSG)
-    puts UNIQ_VIEWS_HEADING_MGS
-    write_statistics_to_console(sorted_by_uniq_views, :uniq_views, UNIQ_VIEWS_LINE_MSG)
-  end
-
-  private
-
-  def write_statistics_to_console(page_views, counter_key, line_message)
-    page_views.each { |page_view| puts "#{page_view.page} #{page_view[counter_key]} #{line_message}" }
+  def self.call(page_views, counter_key, messages)
+    puts messages[:header_msg]
+    page_views.each { |page_view| puts "#{page_view.page} #{page_view[counter_key]} #{messages[:item_msg]}" }
     puts
   end
 end

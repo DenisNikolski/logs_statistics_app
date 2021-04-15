91130c458d7426aa8b1348231bc07fb03f5309bd
DenisNikolski <dzianisnikolski@gmail.com>
2021-04-15 22:56:10 +0300

    refactor Sorter

--- lib/sorter.rb
+++ lib/sorter.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class Sorter
-  def sort(pages_views, criteria)
-    pages_views.sort_by(&criteria).reverse
+  def initialize(pages_views)
+    @page_views = pages_views
+  end
+
+  def call(criteria)
+    sort(criteria)
+  end
+
+  private
+
+  def sort(criteria)
+    @page_views.sort_by { |page_view| -page_view[criteria] }
   end
 end
